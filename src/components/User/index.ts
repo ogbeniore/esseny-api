01df4dd55569b05f6d6cd6b31605ba9b483fc3ba
Oreoluwa Ojo <oreeboy@gmail.com>
2022-09-04 23:08:49 +0200

    initial setup

---
+++ src/components/User/index.ts
@@ -0,0 +1,102 @@
+import { NextFunction, Request, Response } from 'express';
+import UserService from './service';
+import { HttpError } from '../../config/error';
+import { IUserModel } from './model';
+/**
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ * @returns {Promise < void >}
+ */
+export async function findAll(req: Request, res: Response, next: NextFunction): Promise < void > {
+    try {
+        const users: IUserModel[] = await UserService.findAll();
+
+        res.status(200).json(users);
+    } catch (error) {
+        next(new HttpError(error.message.status, error.message));
+    }
+}
+
+/**
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ * @returns {Promise < void >}
+ */
+export async function findOne(req: Request, res: Response, next: NextFunction): Promise < void > {
+    try {
+        const user: IUserModel = await UserService.findOne(req.params.id);
+
+        res.status(200).json(user);
+    } catch (error) {
+        next(new HttpError(error.message.status, error.message));
+    }
+}
+
+interface AuthRequest extends Request {
+    user: IUserModel
+}
+
+/**
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ * @returns {Promise < void >}
+ */
+export async function findOneByEmail(req: AuthRequest, res: Response, next: NextFunction): Promise < void > {
+    try {
+        const {
+            _id, email, picture, name,
+        } = await UserService.findOneByEmail(req.user.email);
+
+        const profile = {
+            _id,
+            email,
+            picture,
+            name,
+        };
+
+        res.status(200).json(profile);
+    } catch (error) {
+        next(new HttpError(error.message.status, error.message));
+    }
+}
+
+/**
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ * @returns {Promise < void >}
+ */
+
+export async function create(req: Request, res: Response, next: NextFunction): Promise < void > {
+    try {
+        const user: IUserModel = await UserService.insert(req.body);
+
+        res.status(201).json(user);
+    } catch (error) {
+        next(new HttpError(error.message.status, error.message));
+    }
+}
+
+/**
+ * @export
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ * @returns {Promise < void >}
+ */
+export async function remove(req: Request, res: Response, next: NextFunction): Promise < void > {
+    try {
+        const user: IUserModel = await UserService.remove(req.params.id);
+
+        res.status(200).json(user);
+    } catch (error) {
+        next(new HttpError(error.message.status, error.message));
+    }
+}
